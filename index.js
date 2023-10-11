c4b04427395ed668f68deb1f293f7f68e3f7a97f
abdullohasia <tolaganova624@gmail.com>
2023-10-11 18:54:57 +0500

    kirma

---
+++ index.js
@@ -0,0 +1,68 @@
+// //  for_1
+//  {
+//   let k = prompt("Birinchi raqamni kiriting"); 
+//   let n = prompt("Ikkinchi raqamni kiriting");
+//   if (n > 0) {
+//     for (i = 0; i <= n; i++) {
+//       console.log(k, i);
+//     }
+//   }
+// }
+
+
+
+
+//  for_2
+ 
+//   let a = prompt("Birinchi raqamni kiriting"); 
+//   let b = prompt("Ikkinchi raqamni kiriting");
+//   if (a < b) {
+//     for ( let i = b; i <= a; i++) {
+//       console.log(a);
+//     }
+//   }
+
+
+
+
+//  for_3
+
+// a = prompt();
+// b = prompt();
+// for (let i = b - 1; i > a; i--) {
+//   console.log(i);
+// }
+// let chiqarilganSonlarSoni = b - a - 1;
+// console.log( sonlarni soni:", SonlarSoni);
+
+
+
+
+// for_4
+
+// let price = parseInt(prompt("narhni kiriting"));
+// for (i = 1; i <= 10; i++) {
+//   res = price * i;
+//   console.log(res);
+// }
+
+
+
+
+// for_5
+
+// let a = parseInt(prompt("Raqamni kiriting"))
+// for (let i=0.1; i<=0.9; i++){
+//   res= a * i;
+//   console.log(res);
+// }
+
+
+
+// for_6
+
+// let a = parseInt(prompt("enter the number"));
+// for (var i = 1.1; i <= 9.9; i++) {
+//   res = a / i;
+//   console.log(i);
+// }
